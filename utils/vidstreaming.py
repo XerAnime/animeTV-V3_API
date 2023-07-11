cc6f819dfb9cd7e053e023d6d0a3a391d09b9a07
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2023-07-11 16:58:15 +0530

    Add files via upload

--- utils/vidstreaming.py
+++ utils/vidstreaming.py
@@ -23,13 +23,25 @@ def unpad(data):
   padding_len = data[-1]
   return data[:-padding_len]
 
+def urlParser(url): 
+  protocol = url.split("://")[0]
+  urldict = {
+    "protocol" : protocol,
+    "hostname" : url.strip(protocol+"://").split("/")[0],
+    "params" : [{ x.split("=")[0] : x.split("=")[1]} for x in url.split("?")[1].split("&")]
+  }
+  return urldict
+
 def generate_encrypted_parameters(url):
-  vid_id = url.split("?")[1].split("&")[0].split("=")[1].strip("=")
+  urlDict = urlParser(url)
+  print(urlDict)
+  url1 = f"{urlDict['protocol']}://{urlDict['hostname']}/streaming.php?id={urlDict['params'][0]['id']}" 
+  vid_id = url.split("?")[1].split("&")[0].split("=")[1]
   cipher_key = AES.new(key, AES.MODE_CBC, iv)
   padded_key = pad(vid_id.encode(), AES.block_size)
   encrypted_key = cipher_key.encrypt(padded_key)
   encoded_key = base64.b64encode(encrypted_key).decode()
-  script = get_video_id(url)
+  script = get_video_id(url1)
   decoded_script = base64.b64decode(script)
   cipher_script = AES.new(key, AES.MODE_CBC, iv)
   decrypted_script = unpad(cipher_script.decrypt(decoded_script))
@@ -45,20 +57,11 @@ def decrypt_encrypted_response(response_data):
   decrypted_text = unpadded_data.decode('utf-8')
   return decrypted_text
 
-def urlParser(url): 
-  protocol = url.split("://")[0]
-  urldict = {
-    "protocol" : protocol,
-    "hostname" : url.strip(protocol+"://").split("/")[0],
-    "params" : [{ x.split("=")[0] : x.split("=")[1]} for x in url.split("?")[1].split("&")]
-  }
-  return urldict
-
 def getEpStreamingLink(iframeUrl):
   # if "typesub" in iframeUrl:
   #   pass
   # else:
-  #   iframeUrl = iframeUrl + "&typesub=DUB"
+  #   iframeUrl = iframeUrl + "&typesub=DUB" 
   urldict = urlParser(iframeUrl)
   USER_AGENT = request_headers()["User-Agent"]
   encrypted_params = generate_encrypted_parameters(iframeUrl)
@@ -66,4 +69,4 @@ def getEpStreamingLink(iframeUrl):
   headers = {"User-Agent" : USER_AGENT,"X-Requested-With": "XMLHttpRequest"}
   response = requests.get(request_url,headers = headers)
   decryptedJson = decrypt_encrypted_response(response.json()["data"])
-  return json.loads(decryptedJson) 
+  return json.loads(decryptedJson)
